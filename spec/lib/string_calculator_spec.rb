2e2aa81381661cc03f3c060bfff4a597f890c0c2
Poornima2212 <75310535+Poornima2212@users.noreply.github.com>
2025-01-03 23:07:11 +0530

    Step 2: Single number

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+    numbers.to_i
   end
 end
 
@@ -11,6 +12,11 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
+    end
+
+    it "returns the number itself for a single number" do
+        calculator = StringCalculator.new
+        expect(calculator.add("1")).to eq(1)
     end
   end
 end
