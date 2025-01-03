99971d14b3a7434407682df358534b1b6ef70ed4
Poornima2212 <75310535+Poornima2212@users.noreply.github.com>
2025-01-03 23:09:55 +0530

    Step 4: Multiple Numbers

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -23,5 +23,10 @@ RSpec.describe StringCalculator do
         calculator = StringCalculator.new
         expect(calculator.add("1,5")).to eq(6)
     end
+
+    it "returns the sum of multiple numbers separated by commas" do
+        calculator = StringCalculator.new
+        expect(calculator.add("1,2,3,4")).to eq(10)
+    end
   end
 end
